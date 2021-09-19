a963a1b1d495c61661f48f4ca1835b6465ecaa61
Retype GitHub Action <retype@object.net>
2021-09-19 17:37:40 +0000

    Refreshes Retype-generated documentation.
    Process triggered by LaggerP.

--- static/paydata.js
+++ static/paydata.js
@@ -1,4 +1,7 @@
 // Example request structure to be defined
 
-let payData = {}
+let payData = {
+   id_suscripcion: "123", //id de la suscripción de la base de datos de Suscripciones. 
+   status: "PAGADO" | "DEMORADO" | "NO_PAGADO" //posibles estados despues de realizar el pago de la suscripción.
+}
 
