5b2a64eb137490ecd5913b329072438aa979d53f
Pablo Lagger <pablo_lagger@hotmail.com>
2021-10-15 12:57:34 -0300

    Update internalSubscriptionData.js

--- static/internalSubscriptionData.js
+++ static/internalSubscriptionData.js
@@ -4,8 +4,8 @@ let payload = {
     "userId": 1,
     "email":"string", //User Email
     "name": "string", //Package name
-    "subscriptionId": 1,
-    "packageId": 4,
+    "subscriptionId": 1, //id obtenido del context de React (una subscriptionId tiene varios paquetes asociados)
+    "packageId": 4, //este valor puede variar
     "cost": 300,
     "firstName":"string",
     "lastName":"string",
